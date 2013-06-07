adca150d9cd2839186a9f79df22a0b0e59bb27fd
Ian Ynda-Hummel <ianynda@gmail.com>
2013-06-07 12:50:27 -0400

    Initial plugin layout. Capture selected comment blocks for formatting.

---
+++ XCommentWrap/CWPlugin.h
@@ -0,0 +1,13 @@
+//
+//  CWPlugin.h
+//  XCommentWrap
+//
+//  Created by Ian Ynda-Hummel on 6/6/13.
+//  Copyright (c) 2013 Ian Ynda-Hummel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CWPlugin : NSObject
+
+@end
---
+++ XCommentWrap/CWPlugin.m
@@ -0,0 +1,139 @@
+//
+//  CWPlugin.m
+//  XCommentWrap
+//
+//  Created by Ian Ynda-Hummel on 6/6/13.
+//  Copyright (c) 2013 Ian Ynda-Hummel. All rights reserved.
+//
+
+#import "CWPlugin.h"
+
+@interface CWPlugin ()
+@property (nonatomic, strong) NSRegularExpression *singleLinePrefixExpression;
+@property (nonatomic, strong) NSRegularExpression *multiLineStartPrefixExpression;
+@property (nonatomic, strong) NSRegularExpression *multiLinePrefixExpression;
+
+@property (nonatomic, assign) BOOL wrapping;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)notification;
+- (void)textDidChange:(NSNotification *)notification;
+@end
+
+@implementation CWPlugin
+
+#pragma mark Lifecycle
+
++ (void)pluginDidLoad:(NSBundle *)pluginBundle {
+    static CWPlugin *sharedPlugin = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedPlugin = [[CWPlugin alloc] init];
+    });
+}
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        _wrapping = NO;
+
+        [NSNotificationCenter.defaultCenter addObserver:self
+                                               selector:@selector(applicationDidFinishLaunching:)
+                                                   name:NSApplicationDidFinishLaunchingNotification
+                                                 object:nil];
+    }
+    return self;
+}
+
+- (void)dealloc {
+    [NSNotificationCenter.defaultCenter removeObserver:self];
+    [_singleLinePrefixExpression release];
+    [_multiLineStartPrefixExpression release];
+    [_multiLinePrefixExpression release];
+    [super dealloc];
+}
+   
+#pragma mark Notification Handlers
+
+- (void)applicationDidFinishLaunching:(NSNotification *)notification {
+    [NSNotificationCenter.defaultCenter removeObserver:self
+                                                  name:NSApplicationDidFinishLaunchingNotification
+                                                object:nil];
+
+    static NSString *simplePrefixPattern = @"^\\s*(//)\\s*";
+    static NSString *multiLineStartPattern = @"^\\s*(/\\*)\\s*";
+    static NSString *multiLinePattern = @"^(\\s*(\\*)\\s*)";
+
+    self.singleLinePrefixExpression = [NSRegularExpression regularExpressionWithPattern:simplePrefixPattern options:0 error:NULL];
+    self.multiLineStartPrefixExpression = [NSRegularExpression regularExpressionWithPattern:multiLineStartPattern options:0 error:NULL];
+    self.multiLinePrefixExpression = [NSRegularExpression regularExpressionWithPattern:multiLinePattern options:0 error:NULL];
+
+    [NSNotificationCenter.defaultCenter addObserver:self
+                                           selector:@selector(textDidChange:)
+                                               name:NSTextDidChangeNotification
+                                             object:nil];
+}
+
+- (void)textDidChange:(NSNotification *)notification {
+    if (self.wrapping) return;
+
+    static NSString *sourceEditorClassName = @"DVTSourceTextView";
+    if (![notification.object isKindOfClass:NSClassFromString(sourceEditorClassName)]) return;
+
+    NSTextView *textView = (NSTextView *)notification.object;  
+
+    if (textView.selectedRanges.count != 1) return;
+
+    NSRange selectedRange = [textView.selectedRanges[0] rangeValue];
+    NSRange paragraphRange = [textView.textStorage.string paragraphRangeForRange:selectedRange];
+
+    if (paragraphRange.location == NSNotFound) return;
+
+    NSString *paragraphString = [textView.textStorage.string substringWithRange:paragraphRange];
+    NSMutableString *commentBlock = [[NSMutableString alloc] initWithString:paragraphString];
+
+    NSRange previousParagraphRange = { .location = paragraphRange.location - 1, .length = 0 };
+    while (previousParagraphRange.location < textView.textStorage.string.length) {
+        previousParagraphRange = [textView.textStorage.string paragraphRangeForRange:previousParagraphRange];
+        if (previousParagraphRange.location == NSNotFound) break;
+
+        paragraphString = [textView.textStorage.string substringWithRange:previousParagraphRange];
+        if (![self commentPrefixWithLineString:paragraphString]) break;
+
+        [commentBlock insertString:paragraphString atIndex:0];
+
+        previousParagraphRange.location = previousParagraphRange.location - 1;
+        previousParagraphRange.length = 0;
+    }
+
+    NSRange nextParagraphRange = { .location = paragraphRange.location + paragraphRange.length, .length = 0 };
+    while (nextParagraphRange.location < textView.textStorage.string.length) {
+        nextParagraphRange = [textView.textStorage.string paragraphRangeForRange:nextParagraphRange];
+        if (nextParagraphRange.location == NSNotFound) break;
+
+        paragraphString = [textView.textStorage.string substringWithRange:nextParagraphRange];
+        if (![self commentPrefixWithLineString:paragraphString]) break;
+
+        [commentBlock appendString:paragraphString];
+
+        nextParagraphRange.location = nextParagraphRange.location + nextParagraphRange.length;
+        nextParagraphRange.length = 0;
+    }
+}
+
+- (NSString *)commentPrefixWithLineString:(NSString *)lineString {
+    NSRange lineStringRange = NSMakeRange(0, lineString.length);
+    NSTextCheckingResult *singleLinePrefixMatch = [self.singleLinePrefixExpression firstMatchInString:lineString options:0 range:lineStringRange];
+    if (singleLinePrefixMatch) return [lineString substringWithRange:singleLinePrefixMatch.range];
+
+    NSTextCheckingResult *multiLinePrefixMatch = [self.multiLinePrefixExpression firstMatchInString:lineString options:0 range:lineStringRange];
+    if (multiLinePrefixMatch) return [lineString substringWithRange:multiLinePrefixMatch.range];
+
+    NSTextCheckingResult *multiLineStartPrefixMatch = [self.multiLineStartPrefixExpression firstMatchInString:lineString options:0 range:lineStringRange];
+    if (!multiLineStartPrefixMatch) return nil;
+
+    NSString *prefix = [lineString substringWithRange:multiLineStartPrefixMatch.range];
+    prefix = [prefix stringByReplacingOccurrencesOfString:@"/" withString:@" " options:0 range:NSMakeRange(0, prefix.length)];
+    return prefix; 
+}
+
+@end
