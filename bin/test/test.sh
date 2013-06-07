9e3439008f863aefbb0eeec4d6272bb6042d568d
Ian Ynda-Hummel <ianynda@gmail.com>
2013-06-07 14:51:29 -0400

    Update the test to not generate unnecessary files.

--- bin/test/test.sh
+++ bin/test/test.sh
@@ -2,11 +2,9 @@
 
 echo -n "Wraps to correct column boundary: "
 
-cat test1.input | emacs --script ../format.el c++-mode > tmp 2>/dev/null
-if [ "$(diff tmp test1.output)" = "" ];
+if [ "$(cat test1.input | emacs --script ../format.el c++-mode 2>/dev/null)" = "$(cat test1.output)" ];
 then
     echo -e "\033[0;32mpass\033[0m"
 else
     echo -e "\033[0;31mfail\033[0m"
 fi
-rm tmp
